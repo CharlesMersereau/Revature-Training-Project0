73ca22293421fc3c379160cf095933496a82470d
Charles Mersereau <mersereau90@gmail.com>
2020-01-12 20:25:53 -0500

    log4j and driver

---
+++ src/main/java/com/revature/driver/CarLotDriver.java
@@ -0,0 +1,12 @@
+package com.revature.driver;
+
+import com.revature.util.LoggerUtil;
+
+public class CarLotDriver {
+
+	public static void main(String[] args) {
+		LoggerUtil logger = new LoggerUtil();
+		logger.info("Hello World");
+	}
+
+}
